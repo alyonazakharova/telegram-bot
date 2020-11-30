1f547c3c4d3dbe4a2d4fe11d6cbdebe594791958
Alyona Zakharova <alenazakharovaspb@yandex.ru>
2020-11-30 18:11:12 +0300

    statistics by country name

--- app.py
+++ app.py
@@ -16,9 +16,20 @@ bot = telebot.TeleBot(token)
 
 commands = {'start': 'Start using chatbot',
             'statistics': 'COVID-19 statistics in the world for today',
-             'country': 'COVID-19 statistics in specified country *currently unavailable*',
+             'country': 'COVID-19 statistics in specified country',
             'help': 'Some useful info about chatbot',
             'contacts': 'Developers contacts'}
+
+user_steps = {}
+known_users = []
+
+def get_user_step(uid):
+    if uid in user_steps:
+        return user_steps[uid]
+    else:
+        known_users.append(uid)
+        user_steps[uid] = 0
+        return user_steps[uid]
 
 
 def send_action(action):
@@ -44,15 +55,47 @@ def start_command_handler(message):
     help_command_handler(message)
 
 
-# @bot.message_handler(commands=['country'])
-# @send_action('typing')
-# def country_command_handler(message):
-#     cid = message.chat.id
-#     user_steps[cid] = 1
-#     bot.send_message(cid, '{0}, enter country name, please'.format(message.chat.username))
+@bot.message_handler(commands=['country'])
+@send_action('typing')
+def country_command_handler(message):
+    cid = message.chat.id
+    user_steps[cid] = 1
+    bot.send_message(cid, 'Please, enter country name.'.format(message.chat.username))
+#     кек, короче если выбрать эту команду, то пока не введешь корректное название страны,
+#     он не будет воспринимать другие команды
 
 
-def get_country_info(latitude, longitude):
+@bot.message_handler(func=lambda message: get_user_step(message.chat.id) == 1)
+@send_action('typing')
+def country_statistics_command_handler(message):
+    cid = message.chat.id
+    country_name = message.text.strip().lower()
+
+    statistics = get_info_by_country_name(country_name)
+
+    if statistics is None:
+        with codecs.open('template/not_found.html', 'r', encoding='UTF-8') as file:
+            template = Template(file.read())
+        bot.send_message(cid, template.render(), parse_mode='HTML')
+        return
+
+    user_steps[cid] = 0
+    # дублирование ето круто!11!
+    with codecs.open('template/country_statistics.html', 'r', encoding='UTF-8') as file:
+        template = Template(file.read())
+        reply = template.render(date=date.today(),
+                                country=statistics['Country'],
+                                new_confirmed=statistics['NewConfirmed'],
+                                total_confirmed=statistics['TotalConfirmed'],
+                                new_deaths=statistics['NewDeaths'],
+                                total_deaths=statistics['TotalDeaths'],
+                                new_recovered=statistics['NewRecovered'],
+                                total_recovered=statistics['TotalRecovered'])
+
+    bot.send_message(cid, reply, parse_mode='HTML')
+
+
+def get_info_by_location(latitude, longitude):
     url = "https://api.covid19api.com/summary"
     response = requests.request("GET", url).json()
 
@@ -66,12 +109,21 @@ def get_country_info(latitude, longitude):
             return elem
 
 
+def get_info_by_country_name(country_name):
+    url = "https://api.covid19api.com/summary"
+    response = requests.request("GET", url).json()
+    for elem in response['Countries']:
+        if elem['Country'].lower() == country_name:
+            return elem
+    return None
+
+
 @bot.message_handler(content_types=['location'])
 @send_action('typing')
 def geo_command_handler(message):
     cid = message.chat.id
     # может ли не быть страны в списке?)))
-    country_info = get_country_info(message.location.latitude, message.location.longitude)
+    country_info = get_info_by_location(message.location.latitude, message.location.longitude)
 
     with codecs.open('template/country_statistics.html', 'r', encoding='UTF-8') as file:
         template = Template(file.read())
