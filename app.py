2c42d17568161705549345db1cd0570102cd84ac
Alyona Zakharova <alenazakharovaspb@yandex.ru>
2020-11-30 15:58:45 +0300

    statistics with new api

--- app.py
+++ app.py
@@ -4,14 +4,27 @@ import codecs
 from functools import wraps
 from telebot import types
 from jinja2 import Template
+from services.country_service import CountryService
+from services.statistics_service import StatisticsService
+
+import requests
+from datetime import date
+
 
 token = os.getenv('API_BOT_TOKEN')
 bot = telebot.TeleBot(token)
+user_steps = {}
+known_users = []
+country_service = CountryService()
+stats_service = StatisticsService()
+
+
 commands = {'start': 'Start using chatbot',
             'country': 'Please, enter county name',
             'statistics': 'Statistics by users queries',
             'help': 'Some useful info about chatbot',
             'contacts': 'Developer contacts'}
+
 
 def send_action(action):
 
@@ -35,6 +48,45 @@ def start_command_handler(message):
                      .format(message.chat.username),
                      reply_markup=markup)
     help_command_handler(message)
+
+
+# @bot.message_handler(commands=['country'])
+# @send_action('typing')
+# def country_command_handler(message):
+#     cid = message.chat.id
+#     user_steps[cid] = 1
+#     bot.send_message(cid, '{0}, enter country name, please'.format(message.chat.username))
+
+
+# @bot.message_handler(content_types=['location'])
+# @send_action('typing')
+# def geo_command_handler(message):
+#     cid = message.chat.id
+#     geo_result = country_service.get_country_information(message.location.latitude, message.location.longitude)
+#     statistics = stats_service.get_statistics_by_country_name(geo_result['countryName'], message.chat.username)
+#     user_steps[cid] = 0
+#     bot.send_message(cid, statistics, parse_mode='HTML')
+
+
+@bot.message_handler(commands=['statistics'])
+@send_action('typing')
+def geo_command_handler(message):
+    cid = message.chat.id
+
+    url = "https://api.covid19api.com/summary"
+    response = requests.request("GET", url).json()
+
+    with codecs.open('template/statistics.html', 'r', encoding='UTF-8') as file:
+        template = Template(file.read())
+        reply = template.render(date=date.today(),
+                                confirmed_new=response['Global']['NewConfirmed'],
+                                confirmed_total=response['Global']['TotalConfirmed'],
+                                deaths_new=response['Global']['NewDeaths'],
+                                deaths_total=response['Global']['TotalDeaths'],
+                                recovered_new=response['Global']['NewRecovered'],
+                                recovered_total=response['Global']['TotalRecovered'])
+
+    bot.send_message(cid, reply, parse_mode='HTML')
 
 
 @bot.message_handler(func=lambda message: message.text.lower() == 'hi')
@@ -63,9 +115,7 @@ def help_command_handler(message):
     for command in commands:
         help_text += '/' + command + ': '
         help_text += commands[command] + '\n'
-    help_text += 'NB: I speak only English'
     bot.send_message(cid, help_text)
-
 
 
 if __name__ == '__main__':
